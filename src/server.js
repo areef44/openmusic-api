c4fef00dfabb4453020a2442ae7af8c385c3a1b5
areef44 <areef.empat.empat@gmail.com>
2023-06-09 15:42:03 +0700

    Songs inMemory Structure

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const Hapi = require('@hapi/hapi');
 const albums = require('./api/albums');
+const songs = require('./api/songs')
 const AlbumsService = require('./services/inMemory/AlbumsService');
+const SongsService = require('./services/inMemory/SongsServices');
 const AlbumsValidator = require('./validator/albums');
+const SongsValidator = require('./validator/songs');
  
 const init = async () => {
   const albumsService = new AlbumsService();
+  const songsService = new SongsService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -16,10 +20,10 @@ const init = async () => {
   });
 
   await server.register({
-    plugin: albums,
+    plugin: albums,songs,
     options: {
-        service: albumsService,
-        validator: AlbumsValidator,
+        service: albumsService, songsService,
+        validator: AlbumsValidator, SongsValidator,
     },
   });
  
